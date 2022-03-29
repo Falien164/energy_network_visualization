9534183a68de39f23e66923e8358d033ed18834a
Falien164 <33525024+Falien164@users.noreply.github.com>
2022-03-29 11:05:23 +0200

    add visualization of clusters
    (before refactoring)

--- viewVisualizer.py
+++ viewVisualizer.py
@@ -1,5 +1,7 @@
+import enum
 import numpy as np
 import plotly.graph_objects as go
+from skimage.color import hsv2rgb
 
 from sklearn.cluster import KMeans
 
@@ -9,8 +11,9 @@ class ViewVisualizer():
 
     def __init__(self):
         self.dataLoader = DataLoader("task_data.hdf5")
+        self.clusteredLabels = []
 
-    def create_stylesheet(self, hour):
+    def create_stylesheet(self, hour, num_of_groups):
         stylesheet = [
             {
                 'selector': 'node',
@@ -21,7 +24,7 @@ class ViewVisualizer():
         ]
         
         stylesheet += self.color_nodes(hour, stylesheet)
-        stylesheet += self.draw_arrows_on_edges(hour, stylesheet)
+        stylesheet += self.draw_arrows_on_edges(stylesheet, num_of_groups)
 
         return stylesheet
 
@@ -31,10 +34,8 @@ class ViewVisualizer():
             elements.append({'data': {'id' : str(int(node[0])), 'label' : str(int(node[0])) }})
 
         for branch in self.dataLoader.get_data(hour, 'branches'):
-            a = str(int(branch[0]))
-            b = str(int(branch[1]))
+            a, b = str(int(branch[0])), str(int(branch[1]))
             elements.append({'data': {'id': f'{a}_{b}', 'source' : a, 'target' : b }})
-
         return elements
 
     def color_nodes(self,hour, stylesheet):
@@ -88,18 +89,19 @@ class ViewVisualizer():
 
         return stylesheet
 
-    def draw_arrows_on_edges(self, hour, stylesheet):
-        branches = self.dataLoader.get_data(hour, "branches")
-        for branch in branches:
+    def draw_arrows_on_edges(self, stylesheet, num_of_groups):
+        colors = self.get_random_set_of_colors(num_of_groups)
+        for branch in self.clusteredLabels:
+            color = self.rgb_to_hex(colors[branch[3]][0],colors[branch[3]][1],colors[branch[3]][2])
             if branch[2] > 0:
                 stylesheet.append(                       
                     {
                         'selector': f'#{int(branch[0])}_{int(branch[1])}',
                         'style': {
-                            'mid-target-arrow-color': 'red',
                             'mid-target-arrow-shape': 'vee',
+                            'mid-target-arrow-color': f'#{color}',
                             'arrow-scale': 3,
-                            'line-color': 'blue'
+                            'line-color': f'#{color}'
                         }
                     }
                 )
@@ -108,16 +110,14 @@ class ViewVisualizer():
                     {
                         'selector': f'#{int(branch[0])}_{int(branch[1])}',
                         'style': {
-                            'mid-source-arrow-color': 'red',
                             'mid-source-arrow-shape': 'vee',
+                            'mid-source-arrow-color': f'#{color}',
                             'arrow-scale': 3,
-                            'line-color': 'blue'
+                            'line-color': f'#{color}'
                         }
                     }
                 )
         return stylesheet
-        
-
 
     def group_edges(self,hour, num_groups):
         """
@@ -129,6 +129,10 @@ class ViewVisualizer():
         flow = np.array([branch[2] for branch in branches]).reshape(-1,1)
         clustering = KMeans(n_clusters=num_groups).fit(flow)
 
+        self.clusteredLabels = []       # save clustered edges to color them later
+        for idx, branch in enumerate(branches):
+            self.clusteredLabels.append([int(branch[0]), int(branch[1]), branch[2], clustering.labels_[idx]])
+
         res = ''
         for group in range(0,num_groups):
             res += f'Grupa: {group} o punkcie centralnym: {np.round(clustering.cluster_centers_[group][0],2)} \n'
@@ -137,8 +141,22 @@ class ViewVisualizer():
                     res += f"Linia '{int(branches[idx][0])}-{int(branches[idx][1])}' {np.round(branches[idx][2],2)}MW, "
             res = res[:-2]
             res += '\n\n'
+
         return res
 
+    def rgb_to_hex(self, r, g, b):
+        return ('{:02X}{:02X}{:02X}').format(r, g, b)
+
+    def get_random_set_of_colors(self, n):
+        '''Returns a list of distinct RGB color'''
+        h = np.array([1*i/(n+1) for i in range(n)]).reshape(-1,1)
+        s = np.random.uniform(low=0.5, high=1, size=(n, 1))
+        v = np.random.uniform(low=0.8, high=1, size=(n, 1))
+        hsv_colors = np.transpose([h, s, v], (1, 2, 0))
+        rgb_colors = hsv2rgb(hsv_colors)
+        rgb_colors = (255*(rgb_colors - np.min(rgb_colors))/np.ptp(rgb_colors)).astype(int)  
+        return rgb_colors[:,0,:]
+    
     def highlight_histogram(self, hour, node):
         nodes = self.dataLoader.get_data(hour, 'nodes')
         demand = np.array([n[2] for n in nodes])
